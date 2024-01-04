00aebaf9142361fdff86d5765430f36e6d55c4cb
gabrielmj23 <gabrielmj2312@gmail.com>
2024-01-03 22:14:25 -0400

    Mejorar esquema reporte

--- src/schemas/reporte.ts
+++ src/schemas/reporte.ts
@@ -2,11 +2,13 @@ import { z } from "zod";
 
 // Esquema de validacion de reportes
 export const reporteSchema = z.object({
-    id_condominio: z
+    id_condominio: z.coerce
         .number()
         .min(1, "Se debe especificar la id del condominio"),
-    id_usuario: z.number().min(1, "Se debe especificar la id del usuario"),
+    id_usuario: z.coerce
+        .number()
+        .min(1, "Se debe especificar la id del usuario"),
     asunto: z.string().min(1, "El reporte debe tener un asunto").max(255),
     contenido: z.string().min(1, "El reporte debe tener contenido"),
-    url_archivo: z.string().url().optional(),
+    url_archivo: z.string().optional(),
 });
